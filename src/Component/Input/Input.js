3d7cfcde0afbc84ff734415923b8318ff8b10656
nzrnaghme <naghmehnzr95@gmail.com>
2022-08-11 15:22:25 +0430

    amir

--- src/Component/Input/Input.js
+++ src/Component/Input/Input.js
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
+import './Input.css'
 
 const Input = (props) => {
 
-    return(
-        <TextField 
-        id="outlined-basic" 
-        variant="outlined" 
-        label={props.title}
-        helperText={props.errorMessage}  
-
-        style={{backgroundColor:'#FCFCFC', fontSize:'24px', fontFamily: 'bakh'}}
+    return (
+        <TextField
+            id="outlined-basic"
+            variant="outlined"
+            label={props.title}
+            helperText={props.errorMessage}
+            className='inputModel'
+            style={{ backgroundColor: '#FCFCFC', fontSize: '24px', fontFamily: 'bakh' }}
         />
     );
-
 }
 
-export {Input};
+export { Input };
