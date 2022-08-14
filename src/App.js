5a8e8dcbdda21bc3f59d685b108ac85edc17ee74
nzrnaghme <naghmehnzr95@gmail.com>
2022-08-14 21:10:20 +0430

    rtl material ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
 import { Home } from "./Pages/Home";
 import { Header } from "./Component/Header";
 import { Footer } from './Component/Footer'
+
 import "./App.css";
 import "./Fonts/fonts.css";
 
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-
 function App() {
+
+  const theme = createTheme({
+    direction: 'rtl',
+  });
+
+  const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer></Footer>
-    </div>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <Home />
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
--- src/Component/Button/Btn.js
+++ src/Component/Button/Btn.js
@@ -5,47 +5,47 @@ import { createTheme } from '@mui/material/styles';
 
 const Btn = (props) => {
 
-    const theme = createTheme({
-      palette: {
-        info: {
-          main: '#00ADEF',
-          light: '#00ADEF',
-          dark: '#2AC4FF',
-          contrastText: '#fff',
-        },
-        goal: {
-          main: '#04A641',
-          light: '#04A641',
-          dark: '#06D152',
-          contrastText: '#fff',
-        },
-        detail: {
-          main: '#2693FF',
-          light: '#2693FF',
-          dark: '#2AB1FF',
-          contrastText: '#fff',
-        },
+  const theme = createTheme({
+    palette: {
+      info: {
+        main: '#00ADEF',
+        light: '#00ADEF',
+        dark: '#2AC4FF',
+        contrastText: '#fff',
       },
+      goal: {
+        main: '#04A641',
+        light: '#04A641',
+        dark: '#06D152',
+        contrastText: '#fff',
+      },
+      detail: {
+        main: '#2693FF',
+        light: '#2693FF',
+        dark: '#2AB1FF',
+        contrastText: '#fff',
+      },
+    },
   });
 
   return (
     <ThemeProvider theme={theme} className='btnThemeProvider'>
-      <Button 
-      className='btnButton'
-      variant="contained" 
-      color= {props.color}
-      style={{
-      width:`${props.width}`, 
-      height:`${props.height}`, 
-      fontSize:`${props.fontSize}`, 
-      margin:`${props.margin}`,
-      fontFamily:'bakh'
-    }}
+      <Button
+        className='btnButton'
+        variant="contained"
+        color={props.color}
+        style={{
+          width: `${props.width}`,
+          height: `${props.height}`,
+          fontSize: `${props.fontSize}`,
+          margin: `${props.margin}`,
+          fontFamily: 'bakh'
+        }}
       >
-      {props.text} 
+        {props.text}
       </Button>
     </ThemeProvider>
   );
 }
 
-export {Btn};
+export { Btn };
