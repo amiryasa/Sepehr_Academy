e7e0bf2a064617ab615bff19b4e691d82275362f
Nazar.Naghmeh <Nazar.Naghmeh@Kourosh-AM.com>
2022-10-04 10:44:23 +0330

    shopping 1

---
+++ src/screens/shoppingCart/shoppingCart.js
@@ -0,0 +1,12 @@
+import "./index.css"
+
+export default function ShoppingCart() {
+    return (
+        <div className="shoppingCart">
+            <div className="shoppingCardTitle">
+                <p>سبد خرید</p>
+                <hr></hr>
+            </div>
+        </div>
+    )
+}
