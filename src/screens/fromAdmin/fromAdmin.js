916ab0d792290aed076a5858d8c11a5b2fc105ef
Nazar.Naghmeh <Nazar.Naghmeh@Kourosh-AM.com>
2022-11-18 14:57:02 +0330

    complete style

--- src/screens/fromAdmin/fromAdmin.js
+++ src/screens/fromAdmin/fromAdmin.js
@@ -16,6 +16,7 @@ export default function FormAdmin() {
             setItem('role', role);
 
             navigator("/")
+            window.location.reload(false)
 
             // navigator("/")
         }
